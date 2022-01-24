c98d0c23ab1255946116c70919b3b2c80e94da72
Ivan Vykopal <ivan.vykopal@gmail.com>
2022-01-24 14:00:04 +0100

    Add test data

--- src/model.ts
+++ src/model.ts
@@ -1,15 +1,15 @@
-export type Customer = {
-    id: number;
-    name: string;
-    dateOfBirth: Date;
-    vip: boolean;
-    //totalAmount: number
+export interface Customer {
+    id: number,
+    name: string,
+    dateOfBirth: Date,
+    vip: boolean,
+    totalAmount: number,
 }
 
-export type Order = {
-    id: number;
-    orderDate: Date;
-    amount: number;
-    numberOfProducts: number;
+export interface Order {
+    id: number,
+    orderDate: Date,
+    amount: number,
+    numberOfProducts: number,
 }
 
